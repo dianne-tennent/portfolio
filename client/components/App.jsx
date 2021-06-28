737463eac0080d6e8cf71dc8460781180f5e8d86
Dianne-Tennent <diannerosemay@gmail.com>
2021-06-28 22:41:03 +1200

    Home page set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,17 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-
-import { fetchFruits } from '../actions'
+import React from 'react'
+import HomePage from './HomePage'
 
 function App (props) {
-  useEffect(() => {
-    props.dispatch(fetchFruits())
-  }, [])
+
 
   return (
     <>
       <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {props.fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
+      <HomePage/>
       </div>
     </>
   )
 }
-const mapStateToProps = (globalState) => {
-  return {
-    fruits: globalState.fruits
-  }
-}
 
-export default connect(mapStateToProps)(App)
+
+export default App
